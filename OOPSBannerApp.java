3f6b966617b89e72e44ac215089ee0e43b1f4d66
NamanVerma007 <namanverma2007@gmail.com>
2026-02-16 21:27:02 +0530

    OOPS Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -7,11 +7,11 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****+""+   *****+""+   ****** +""+  ***** ");
-        System.out.println("*     * +""+*     *+""+ *     * +""+*     ");
-        System.out.println("*     * +""+*     *+""+ ******  +""+****** ");
-        System.out.println("*     *+""+ *     *+""+ *       +""+      * ");
-        System.out.println(" ***** +""+  ***** +""+ *      +""+  ***** ");
+        System.out.println(" *****"+""+"   *****"+""+"   ****** "+""+"  ***** ");
+        System.out.println("*     * "+""+"*     *"+""+" *     * "+""+"*     ");
+        System.out.println("*     * "+""+"*     *"+""+" ******  "+""+"****** ");
+        System.out.println("*     *"+""+" *     *"+""+" *       "+""+"      * ");
+        System.out.println(" ***** "+""+"  ***** "+""+" *      "+""+"  ***** ");
 
     }
 }
