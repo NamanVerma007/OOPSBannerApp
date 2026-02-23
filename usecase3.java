04c2ea765839a191921d8073c6d704a39cba52e3
NamanVerma007 <namanverma2007@gmail.com>
2026-02-23 08:26:52 +0530

    updated

---
+++ usecase3.java
@@ -0,0 +1,61 @@
+/**
+ * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
+ * Uses String.join() for better efficiency and readability.
+ * @author Developer
+ * @version 3.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("  ",
+                " *** ",
+                " *** ",
+                " ***** ",
+                " ***** "
+        ));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*"
+        ));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*****",
+                "**** "
+        ));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "   * "
+        ));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "   * "
+        ));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *"
+        ));
+
+        System.out.println(String.join("  ",
+                " *** ",
+                " *** ",
+                "*    ",
+                "**** "
+        ));
+    }
+}
